9ff2dbe6059767e39b5914e72ae7bb0682697307
Jason Mei <jason.mei403@gmail.com>
2019-10-13 15:43:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  let dataObject = {
+    name: name,
+    email: email
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(dataObject)
+  };
+
+  fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(results => {
+      const body = document.querySelector("body");
+      const h2 = document.createElement("h2");
+      h2.innerHTML = results.id;
+      body.appendChild(h2);
+    })
+}

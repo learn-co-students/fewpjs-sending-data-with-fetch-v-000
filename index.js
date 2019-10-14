5c3f9f0ab8880304914e60fe890170957b73d544
Jennifer Pazos <jmp521@gmail.com>
2019-10-14 11:55:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    fetch('http://localhost:3000/users', {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }, 
+        body: JSON.stringify({
+             userName, 
+             userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(json => appendId(json))
+    .catch(error => {
+        document.body.innerHTML = error.message
+    })
+}
+
+function appendId(id) {
+    document.body.innerHTML = id["id"]
+    
+}
+

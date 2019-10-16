dabf9a6c349bba813257c808de5ae13647971818
atxrenegade <harleighabel@gmail.com>
2019-10-16 00:54:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+let configObject = {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	},
+	body: JSON.stringify( {
+	name: 'Steve',
+	email: 'steve@steve.com'
+	})
+};
+
+function addIdToPage(obj) {
+console.log(obj);
+	const el = document.querySelector('body');
+	let idElement = document.createElement('p');
+	idElement.innerHTML = obj.id;
+	el.appendChild(idElement);
+}
+
+function addErrorToDOM(error) {
+	const el = document.querySelector('body');
+	let errorMessage = document.createElement('p');
+	errorMessage.innerHTML = error;
+	el.appendChild(errorMessage);
+}
+
+function submitData(name, email) {
+	return fetch('http://localhost:3000/users', configObject)
+	.then(resp => resp.json())
+	.then(json => addIdToPage(json))
+	.catch(error =>
+		addErrorToDOM(error))
+}

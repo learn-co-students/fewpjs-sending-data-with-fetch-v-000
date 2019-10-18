46d8b6e61a577ff8137fc7c3f0ad0acdce3e9931
AlwinaO <aoyewole2586@gmail.com>
2019-10-17 23:43:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const submitData = function(name, email) {
+
+    const requestBody = {
+        name: name, 
+        email: email
+    };
+
+    const userForm = document.querySelector("#userForm")
+
+    const  configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(requestBody)
+    }; 
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(json => 
+            Object.keys(json).(id) => 
+                const h1 = document.createElement('h1');
+                h1.textContent = id;
+            
+            );
+}
+
+// let ids = json.map(user => {
+//     return `<h1>${user.id}</h1>`
+// })
+// userForm.innerHTML = ids;

bb7b9ef4944389315b106e6d0b32724511d2d8ad
Richard Cho <richardcho100@gmail.com>
2019-11-02 19:06:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(json => displayId(json))
+    .catch(error => displayError(error));
+}
+
+function displayId(json) {
+    const id = json.id
+
+    const p = document.createElement('p');
+    p.textContent = id;
+    document.body.appendChild(p);
+}
+
+function displayError(error) {
+    const p = document.createElement('p');
+    p.textContent = error;
+    document.body.appendChild(p);
+}

1acd41bbee19557b0af9f7c6b3a5e344d7b75737
John Blank <veloblank@gmail.com>
2019-11-07 04:36:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(username, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail
+    })
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (obj) {
+      document.body.innerHTML = obj.id
+    })
+    .catch(function (error) {
+      console.log(error)
+      document.body.innerHTML = error.message
+    })
+}

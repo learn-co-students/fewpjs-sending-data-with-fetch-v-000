e0d81cbe6095b6e7e20bf4c47223f3fcdd044f20
Nicholas Romans <Nicholas.Romans@gmail.com>
2019-11-13 12:06:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-// Add your code here
-function submitData( name, email ) {
-    return fetch( 'http://localhost:3000/users', {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-        },
-        body: JSON.stringify( {
-          name,
-          email
-        })
-      })
+function submitData(name, email) {
+  return fetch ("http://localhost:3000/users", {
+  method: "Post",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name, email
+  })
+})
 
-      .then( function ( response ) {
-        return response.json()
-      })
-
-      .then( function ( object ) {
-        document.body.innerHTML = object[ "id" ]
-      })
-
-      .catch( function ( error ) {
-        document.body.innerHTML = error.message
-      })
-  }
+.then (function(response) {
+  return response.json();
+})
+.then (function(object) {
+  document.body.innerHTML = object[ "id" ]
+})
+.catch(function(error) {
+  alert("Bad things!  Ragnarok!");
+  document.body.innerHTML = error.message;
+})
+};

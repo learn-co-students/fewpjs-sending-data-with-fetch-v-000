f70633d56d6e3eb44941f923d70a348648c68aaa
Daniel Goldberg <danpaulgo@aol.com>
2019-11-19 10:59:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+
+function submitData(name, email){
+	let formData = {
+		name: name,
+		email: email
+	}
+
+	let configObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		body: JSON.stringify(formData)
+	}
+
+	let pageBody = document.querySelector("body")
+	let newSection = document.createElement("div")
+
+	fetch("http://localhost:3000/users", configObj)
+	.then(function(response){
+		// console.log(response)
+		console.log(response.json())
+		// return response.json()
+	}).then(function(object){
+		newSection.innerText = object.id
+		pageBody.appendChild(newSection)
+		console.log(object)
+		return object
+	}).catch(function(error){
+		newSection.innerText = error.message
+		pageBody.appendChild(newSection)
+	})
+}

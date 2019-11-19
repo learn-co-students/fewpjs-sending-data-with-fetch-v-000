00a8183f2d5a736cdf30ca286aa7eddd11e9d6b4
Daniel Goldberg <danpaulgo@aol.com>
2019-11-19 11:12:59 -0500

    Done.

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function submitData(name, email){
 	let pageBody = document.querySelector("body")
 	let newSection = document.createElement("div")
 
-	fetch("http://localhost:3000/users", configObj)
+	return fetch("http://localhost:3000/users", configObj)
 	.then(function(response){
-		console.log(response)
-		console.log(response.json())
+		// console.log(response)
+		// console.log(response.json())
 		return response.json()
 	}).then(function(object){
 		newSection.innerText = object.id

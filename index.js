90494998ba061ed32def5c53178b96546c4086a7
Thomas Gray <thomas.gray@tgray.fios-router.home>
2019-11-18 19:37:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const userObject = {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  const response = fetch("http://localhost:3000/users", userObject)
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(object) {
+      document.getElementsByTagName('body')[0].append(object.id);
+    })
+    .catch(function(error) {
+      document.getElementsByTagName('body')[0].append(error.message);
+    })
+    ;
+
+  return response;
+}

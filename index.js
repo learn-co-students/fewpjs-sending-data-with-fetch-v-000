026b51ead4beaff267aab6d1fa05df7bc4d5f1ec
Meg Gutshall <meghan.gutshall@gmail.com>
2019-12-09 19:21:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  let formData = {
+    name: name,
+    email: email
+  };
+
+  let obj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  function fetchHelper(param) {
+    let element = document.createElement("p");
+    element.innerHTML = param;
+    document.body.appendChild(element);
+  };
+
+  return fetch("http://localhost:3000/users", obj)
+    .then(resp => resp.json())
+    .then(user => fetchHelper(user.id))
+    .catch(error => fetchHelper(error.message));
+};

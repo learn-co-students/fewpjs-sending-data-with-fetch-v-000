50607498c94bdf972676f78534facca74fec4eb7
ElvinGarcia <dimix181@gmail.com>
2020-01-04 14:39:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+
+ function submitData(name, email) {
+    
+    let formData = {
+        name : name,
+        email : email
+    }
+
+    let configObj = { 
+    method: "POST",
+    headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+    },
+        body: JSON.stringify(formData)
+ }
+    return fetch("http://localhost:3000/users", configObj)
+    .then((response)=>{return response.json()})
+        .then( (obj) => {document.body.innerHTML = obj.id} )
+        .catch((error) => { document.body.innerHTML = error.message })
+}

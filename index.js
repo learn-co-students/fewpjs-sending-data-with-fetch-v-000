c60242394d4ac0cedb961f4d1a9ff142ca6b9575
GiancarloZ <gzabaleta48@gmail.com>
2020-01-21 14:20:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      document.body.innerHTML = object[ "id" ];
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    });
+}
+
+   

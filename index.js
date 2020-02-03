8f8c16d3322b02fc4f1602a39f09ab1b6e607dc0
William Barela <william.barela.dev@gmail.com>
2020-02-03 23:35:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+  const url = 'http://localhost:3000/users'
+  
+  let userData = {
+    name: name,
+    email: email
+  };
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch(url, configObj)
+  .then(resp => resp.json())
+  .then(object => document.body.innerHTML = object["id"])
+  .catch(errors => document.body.innerHTML = errors.message)
+}

0c01c31b03992c68b7f7ac14f60f4e6c93af7d1e
Kat Tochowicz <ktochow1@binghamton.edu>
2020-04-01 10:20:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+
+const body = document.getElementById("body");
+
+function submitData(name, email){
+  // console.log(userData)
+  let configObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({name: name, email: email})
+  };
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function(response){
+      return response.json()
+    }).then(function(object){
+      document.body.innerHTML = object.id;
+      // console.log(object)
+    }).catch(function(error){
+      document.body.innerHTML = error.message;
+    })
+};

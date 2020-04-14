1041d082eb4b18e487185c33374ff82d78c92d36
andrewhongUW <andrewtotheh@gmail.com>
2020-04-14 16:22:09 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+const usersURL = 'http://localhost:3000/users'
+
+
+function submitData(name, email) {
+  return fetch(usersURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({name: name, email: email})
+  })
+  .then(response => response.json())
+  .then(json => appendData(json))
+  .catch(error => appendError(error))
+}
+
+const appendData = (json) => {
+  document.body.append(json.id)
+}
+
+const appendError = (error) => {
+  document.body.append(error.message)
+}

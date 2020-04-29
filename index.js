718dd432b0b2f080f2e78330f5837fc5b66813d2
sdcro <sdcrouse91@gmail.com>
2020-04-29 00:17:46 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 // Add your code here
-configurationObject = {
+
+// configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-type": "application/json",
+//     "Accept": "application/json"
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   })
+// };
+
+fetch("http://localhost:3000/dogs", {
   method: "POST",
   headers: {
     "Content-type": "application/json",
@@ -9,4 +22,4 @@ configurationObject = {
     dogName: "Byron",
     dogBreed: "Poodle"
   })
-};
+});

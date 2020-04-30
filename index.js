48a0728a8caca7c269a645c0109c487a45c880c4
sdcro <sdcrouse91@gmail.com>
2020-04-30 16:32:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 // Add your code here
 
-// Example code:
+/* Example code:
 
-// configurationObject = {
-//   method: "POST",
-//   headers: {
-//     "Content-type": "application/json",
-//     "Accept": "application/json"
-//   },
-//   body: JSON.stringify({
-//     dogName: "Byron",
-//     dogBreed: "Poodle"
-//   })
-// };
+  configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      dogName: "Byron",
+      dogBreed: "Poodle"
+    })
+  };
 
-let formData = {
-  dogName: "Soda",
-  dogBreed: "Goldendoodle"
-};
+  let formData = {
+    dogName: "Soda",
+    dogBreed: "Goldendoodle"
+  };
 
-let configObj = {
-  method: "POST",
-  headers: {
-    "Content-type": "application/json",
-    "Accept": "application/json"
-  },
-  body: JSON.stringify(formData)
-};
+  let configObj = {
+    // method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
 
-fetch("http://localhost:3000/dogs", configObj)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(object) {
-    console.log(object);
-  })
-  .catch(function(error) {
-    alert("Bad things! Ragnarok!");
-    console.log(error.message);
-  });
+  fetch("http://localhost:3000/dogs", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarok!");
+      console.log(error.message);
+    });
+
+*/

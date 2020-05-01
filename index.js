86de8427e31580d839af415717ab87518e645a8b
sdcro <sdcrouse91@gmail.com>
2020-04-30 18:51:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 // Add your code here
 
+function displayMessage(message) {
+  const messageElement = document.createElement('p');
+
+  messageElement.textContent = message;
+  document.body.append(messageElement);
+}
+
+// function displayUserId(json) {
+//   displayMessage(`User id: ${json.id}`);
+// }
+
+// function displayError(error) {
+//   displayMessage(error.message);
+// }
+
+function submitData(userName, userEmail) {
+  const destinationUrl = "http://localhost:3000/users";
+
+  let userData = {
+    name: userName,
+    email: userEmail
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch(destinationUrl, configObj)
+           .then(response => response.json())
+           .then(function(json) {
+             displayMessage(`User id: ${json.id}`);
+           })
+           .catch(function(error) {
+             displayMessage(error.message);
+           });
+}
+
 /* Example code:
 
   configurationObject = {

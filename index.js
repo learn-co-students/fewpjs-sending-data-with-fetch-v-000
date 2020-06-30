dddd997dcfac4d91aa5517d15bbf9726dca6c0f1
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2020-06-30 23:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const submitData = (name, email) => {
+  let data = {
+    name: name,
+    email: email
+  }
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+            .then(response => response.json())
+            .then((result) => {
+              document.body.innerHTML = result.id
+            })
+            .catch((error) => {
+              document.body.innerHTML = error.message
+            })
+
+}

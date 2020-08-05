60d9cc50a68363582ebe90c44a43e91e48fc5036
Sara Harvey <saraharveynyc@gmail.com>
2020-08-05 17:46:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+	let formData = {
+	    name: name,
+	    email: email
+  };
+
+	let configObj = {
+	    method: "POST",
+	    // 2. Headers for 'Content-Type' and 'Accept' set to 'application/json'
+	    headers: {
+	      "Content-Type": "application/json",
+	      "Accept": "application/json"
+    },
+    	body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+  .then(response => response.json())
+  .then(object => {
+    document.body.innerHTML = object.id;
+  })
+
+    .catch(function(error) {
+    document.body.innerHTML = error.message;
+  });
+}

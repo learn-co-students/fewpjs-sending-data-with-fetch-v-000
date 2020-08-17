7381cd979e98761e62e488efd1c22f2442861b3c
tscotce <tatyana.celovsky@gmail.com>
2020-08-17 18:06:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+   
+  let submitData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+   
+  fetch("http://localhost:3000/users", submitData)
+    .then(function(response) {
+      return response.json();
+    //   return fetch();
+    })
+    .then(function(object) {
+      console.log(object);
+    });
+    // .catch(function(error) {
+    //     alert("Bad things! Ragnarők!");
+    //     console.log(error.message);
+    // });

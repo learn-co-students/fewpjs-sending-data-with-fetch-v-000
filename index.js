60d727424aac8f05a92f79847249fa6c6623dbf5
Eric Johnson <ericjohnson0216@icloud.com>
2020-09-05 10:41:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+let formData = {
+    name: "Steve",
+    email: "steve@steve.com"
+};
+
+let configObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+};
+
+
+function submitData(name, email) {
+fetch("http://localhost:3000/users", configObj)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (results) {
+        results.configObj.forEach(id => addId(id))
+
+    })
+};

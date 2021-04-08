2faec1542d27d31f366ef74b00c6da2e087aafe8
capricious-slingshot <ephemere@protonmail.com>
2021-04-08 15:33:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+function submitData(n, e){
+  let formData = { name: n, email: e }
+
+  let configurationObject = {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
+      "Accept" : "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(resp =>  resp.json() ).then( function(data) { document.body.innerHTML = data["id"]})
+  .catch(function(err) { document.body.innerHTML = err.message })
+}

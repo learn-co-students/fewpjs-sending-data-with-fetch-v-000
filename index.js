fde35c3bd699d26e73c72b732a4ec437a44ffa88
klovae <elyse.klova@gmail.com>
2021-04-19 22:26:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+  
+
+function submitData(userName, userEmail) {
+
+  return fetch('http://localhost:3000/users', {
+
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail
+    }),
+  })
+
+  .then(function(response) {
+    return response.json();
+  })
+
+  .then(function(object) {
+    let div = document.createElement('div')
+    div.innerHTML = object['id']
+    document.body.appendChild(div)
+  })
+
+  .catch(function(error) {
+    let div = document.createElement('div')
+    div.innerHTML = error.message
+    document.body.appendChild(div)
+  })
+
+}

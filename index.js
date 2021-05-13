ef25883b5a8d3e5a9e044f37599239ec3318127b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-05-13 17:16:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        "name": name,
+        "email": email
+    }
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"];
+        })
+        .catch(function(object){
+            document.body.innerHTML = object.message;
+        })
+}

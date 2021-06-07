faddf29f5b731469ad550e34b74838d0fbe2f29a
villysiu <villysiu@gmail.com>
2021-06-07 09:17:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+
+   
+  // method: "POST" is missing from the object below
+  
+   function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+      fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        console.log(object)
+      renderUser(object);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);   
+    });
+   // return fetch();
+   }
+   function renderUser(user) {
+    const main = document.querySelector('body');
+       // {dogName: "Byron", dogBreed: "Poodle", id: 6} 
+    const p = document.createElement('p');
+    p.innerHTML = user.id;
+    main.appendChild(p)
+
+
+   }
+
+   document.addEventListener('DOMContentLoaded', function() {
+    submitData();
+  });
+  
